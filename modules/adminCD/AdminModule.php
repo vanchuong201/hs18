9b6f6ebddfede8fe2474e8e180b7f7c4f128941d
Chuongnv <vanchuong201@gmail.com>
2017-04-16 21:00:37 +0700

    create file AdminModule

---
+++ modules/adminCD/AdminModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\adminCD;
+
+/**
+ * adminCD module definition class
+ */
+class AdminModule extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\adminCD\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
