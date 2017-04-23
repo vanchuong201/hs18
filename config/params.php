3e8d0eab801f6f24f8a58dac0d9ac1dd410b8fa6
Chuongnv <vanchuong201@gmail.com>
2017-04-23 21:10:27 +0700

    add column fullname and config

--- config/params.php
+++ config/params.php
@@ -2,4 +2,6 @@
 
 return [
     'adminEmail' => 'vanchuong201@gmail.com',
+    'app_name' => 'HẢI SẢN 18',
+    'sidebar-collapse'=>0
 ];
--- views/adminCD/layouts/header.php
+++ views/adminCD/layouts/header.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">HS18</span><span class="logo-lg">' . Yii::$app->params['app_name'] . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
@@ -230,7 +230,7 @@ use yii\helpers\Html;
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="user-image" alt="User Image"/>
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs"><?=Yii::$app->getUser()->identity->fullname ? Yii::$app->getUser()->identity->fullname : Yii::$app->getUser()->identity->username?></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
